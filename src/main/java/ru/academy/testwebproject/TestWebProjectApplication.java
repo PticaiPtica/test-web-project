b1ad670ccc86258c3156674e6f198ef40053a276
Greider <kotx25@yandex.ru>
2025-06-07 12:35:26 +0500

    first commmit

---
+++ src/main/java/ru/academy/testwebproject/TestWebProjectApplication.java
@@ -0,0 +1,13 @@
+package ru.academy.testwebproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestWebProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestWebProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/academy/testwebproject/TestWebProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.academy.testwebproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestWebProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
